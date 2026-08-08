6c66b192e7ae2dc49768ad88c92ba72abc0121a0
Vecha Laxmi Swaraj Babu <syamalaswaraj@gmail.com>
2026-08-08 18:41:54 +0530

    feat: implement Product, ProductTab, and Title components to demonstrate prop usage and data rendering

--- 15 React/1 Basic learning About React/src/Product.jsx
+++ 15 React/1 Basic learning About React/src/Product.jsx
@@ -1,13 +1,18 @@
-import React from 'react'
-import './Product.css'
-
-const Product = () => {
+import "./Product.css";
+// Passing the defalt valu and it works like the functions
+const Product = ({ title, price = 10, options, options1,options2}) => {
   return (
-    <div className='Product'>
-        <h3>Product Title: </h3>
-        <h5>Proguct Desc</h5>
+    <div className="Product">
+      <h3>Product Title: {title}</h3>
+      <p>{options}</p>
+      <p>{options1?.a}</p>
+      <p>{options2}</p>
+
+      {/* another meather to print the arr or acces the array */}
+      <p>{options?.map((featuer)=> <li>{featuer}</li>)}</p>
+      <h5>Product Price: {price}</h5>
     </div>
   );
 };
 
-export {Product};
+export { Product };
--- 15 React/1 Basic learning About React/src/ProductTab.jsx
+++ 15 React/1 Basic learning About React/src/ProductTab.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import { Product } from './Product'
+import { Product } from "./Product";
 
 export const ProductTab = () => {
-    return (
-        <>
-            <Product />
-            <Product />
-            <Product />
-        </>
-    )
-}
+  let arr = ["nice", "good", "babu"];
+  let arr2 = { a: "nice", b: "good", c: "babu" };
+  let arr3 = [
+    <li key="a"> nice </li>,
+    <li key="b"> good </li>,
+    <li key="c"> babu </li>,
+  ];
+  return (
+    <>
+      <Product title="Phone" price={1000} options={arr} options1={arr2} />
+      <Product title="Laptop" price={5000} options2={arr3} />
+      <Product title="Pen" />
+    </>
+  );
+};
